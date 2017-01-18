0b68d1085273e61e347ec08465f24bf6ca17e7c9
Sarah-Wang <yyzxml@outlook.com>
2017-01-18 17:38:15 +0800

    modify awk.sh
    Signed-off-by: Sarah-Wang <yyzxml@outlook.com>

--- awk/awk.sh
+++ awk/awk.sh
@@ -10,7 +10,6 @@ awk  -F '[;:]' '{print $1,$3,$6}' OFS="\t" /etc/passwd
 echo 
 echo "--- count the total size of file in current directory----"
 ls -l | awk '$1~/^[^d]/ {print $NF; sum+=$5} END {print "total file size is: "sum}'
-
 echo
 echo "--- count the memory space users process consumed by RSS ---"
 ps aux | awk 'NR!=1{a[$1]+=$6;} END {for (i in a) print i ": " a[i]"KB"}'
